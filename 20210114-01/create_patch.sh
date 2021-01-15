fd6a5839957158eae305cd47096f6948598e3e40
jhpoelen <jhpoelen@xs4all.nl>
2021-01-15 14:52:28 -0800

    make script a little more modular

--- 20210114-01/create_patch.sh
+++ 20210114-01/create_patch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Patch Related to issues: 
+# Patch Related to issues:
 #  https://github.com/globalbioticinteractions/globi-taxon-names/issues/6
 #  https://github.com/globalbioticinteractions/globalbioticinteractions/issues/569
 #  https://github.com/globalbioticinteractions/globalbioticinteractions/issues/243
@@ -9,16 +9,19 @@
 
 set -xe
 
-mkdir -p input output
+function init {
+  mkdir -p input output
 
-# get GloBI taxon graph 0.3.27
-curl -L https://zenodo.org/record/4118439/files/taxonMap.tsv.gz > input/taxonMap.tsv.gz
-curl -L https://zenodo.org/record/4118439/files/taxonCache.tsv.gz > input/taxonCache.tsv.gz
+  # get GloBI taxon graph 0.3.27
+  curl -L https://zenodo.org/record/4118439/files/taxonMap.tsv.gz > input/taxonMap.tsv.gz
+  curl -L https://zenodo.org/record/4118439/files/taxonCache.tsv.gz > input/taxonCache.tsv.gz
 
-# get nomer v0.1.19 
-curl -L "https://github.com/globalbioticinteractions/nomer/releases/download/0.1.19/nomer.jar" > input/nomer.jar
+  # get nomer v0.1.19
+  curl -L "https://github.com/globalbioticinteractions/nomer/releases/download/0.1.19/nomer.jar" > input/nomer.jar
+}
 
-cat input/taxonMap.tsv.gz \
+function resolve {
+  cat input/taxonMap.tsv.gz \
  | gunzip\
  | grep "NCBI:"\
  | tail -n+2\
@@ -29,7 +32,7 @@ cat input/taxonMap.tsv.gz \
  | gzip\
  > output/ncbi-matches.tsv.gz
 
-cat output/ncbi-matches.tsv.gz \
+  cat output/ncbi-matches.tsv.gz \
  | gunzip\
  | grep "NONE"\
  | cut -f1,2\
@@ -38,29 +41,31 @@ cat output/ncbi-matches.tsv.gz \
  | java -jar input/nomer.jar append globi-globalnames\
  | gzip\
  >> output/gn-matches.tsv.gz
+}
 
-
-cat output/ncbi-matches.tsv.gz output/gn-matches.tsv.gz\
+function merge {
+  # merge newly resolved ncbi taxa into taxon graph
+  cat output/ncbi-matches.tsv.gz output/gn-matches.tsv.gz\
 | gunzip\
 | grep -v NONE\
 | cut -f1,2,4,5\
 | gzip\
 > output/taxonMapNCBI.tsv.gz
 
-cat output/ncbi-matches.tsv.gz output/gn-matches.tsv.gz\
+  cat output/ncbi-matches.tsv.gz output/gn-matches.tsv.gz\
 | gunzip\
 | grep -v NONE\
 | cut -f6-\
 | gzip\
 > output/taxonCacheNCBI.tsv.gz
 
-cat input/taxonCache.tsv.gz\
+  cat input/taxonCache.tsv.gz\
  | gunzip\
  | head -n1\
  | gzip\
  > output/taxonCache1.tsv.gz
 
-cat input/taxonCache.tsv.gz output/taxonCacheNCBI.tsv.gz\
+  cat input/taxonCache.tsv.gz output/taxonCacheNCBI.tsv.gz\
  | gunzip\
  | tail -n+2\
  | sort\
@@ -68,33 +73,41 @@ cat input/taxonCache.tsv.gz output/taxonCacheNCBI.tsv.gz\
  | gzip\
  >> output/taxonCache1.tsv.gz
 
-cat input/taxonMap.tsv.gz\
+  cat input/taxonMap.tsv.gz\
  | gunzip\
  | head -n1\
  | gzip\
  > output/taxonMap1.tsv.gz
 
-cat input/taxonMap.tsv.gz\
+  cat input/taxonMap.tsv.gz\
  | gunzip\
  | tail -n+2\
  | grep -v "NCBI:"\
  | gzip\
  > output/taxonMapNoNCBI.tsv.gz
 
-cat input/taxonMapNoNCBI.tsv.gz output/taxonMapNCBI.tsv.gz\
+  cat input/taxonMapNoNCBI.tsv.gz output/taxonMapNCBI.tsv.gz\
  | gunzip\
  | sort\
  | uniq\
  | gzip\
  >> output/taxonMap1.tsv.gz
+}
 
-diff <(cat input/taxonCache.tsv.gz | gunzip) <(cat output/taxonCache1.tsv.gz| gunzip) | gzip > output/taxonCache.tsv.patch.gz
-diff <(cat input/taxonMap.tsv.gz | gunzip) <(cat output/taxonMap1.tsv.gz| gunzip) | gzip > output/taxonMap.tsv.patch.gz
+function create_patch {
+  diff <(cat input/taxonCache.tsv.gz | gunzip) <(cat output/taxonCache1.tsv.gz| gunzip) | gzip > output/taxonCache.tsv.patch.gz
+  diff <(cat input/taxonMap.tsv.gz | gunzip) <(cat output/taxonMap1.tsv.gz| gunzip) | gzip > output/taxonMap.tsv.patch.gz
 
-zcat input/taxonCache.tsv.gz > output/taxonCacheToBePatched.tsv
-zcat output/taxonCache.tsv.patch.gz | patch -b output/taxonCacheToBePatched.tsv
-cat output/taxonCacheToBePatched.tsv | gzip > output/taxonCachePatched.tsv.gz
+  cat input/taxonCache.tsv.gz | gunzip > output/taxonCacheToBePatched.tsv
+  cat output/taxonCache.tsv.patch.gz | gunzip | patch -b output/taxonCacheToBePatched.tsv
+  cat output/taxonCacheToBePatched.tsv | gzip > output/taxonCachePatched.tsv.gz
 
-zcat input/taxonMap.tsv.gz > output/taxonMapToBePatched.tsv
-zcat output/taxonMap.tsv.patch.gz | patch -b output/taxonMapToBePatched.tsv
-cat output/taxonMapToBePatched.tsv | gzip > output/taxonMapPatched.tsv.gz
+  cat input/taxonMap.tsv.gz | gunzip > output/taxonMapToBePatched.tsv
+  cat output/taxonMap.tsv.patch.gz | gunzip | patch -b output/taxonMapToBePatched.tsv
+  cat output/taxonMapToBePatched.tsv | gzip > output/taxonMapPatched.tsv.gz
+}
+
+init
+resolve
+merge
+create_patch
