e2499e7a8014ae142c0ed50e18e8ce4d288abcaf
jhpoelen <jhpoelen@xs4all.nl>
2021-06-23 17:28:07 -0700

    prepare patch related to https://github.com/globalbioticinteractions/globalbioticinteractions/issues/672

---
+++ 20210623-01/create_patch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Patch Related to issue:
+#  https://github.com/globalbioticinteractions/globalbioticinteractions/issues/672
+#
+
+set -xe
+
+function init {
+  mkdir -p input output
+
+  # get GloBI taxon graph 0.3.32
+  curl -L https://zenodo.org/record/4753955/files/taxonMap.tsv.gz > input/taxonMap.tsv.gz
+}
+
+function remove_likely_suspicious_virus_acronym_mappings {
+  # attempt to remove suspect mappings like [Plutella xylostella NPV] -> [Plutella xylostella]
+  # see https://github.com/globalbioticinteractions/globalbioticinteractions/issues/672
+  cat input/taxonMap.tsv.gz\
+  | gunzip\
+  | grep -P ".*(NCBI).*\t.*V\t"\
+  | gzip\
+  > output/taxonMapNCBI_Vs.tsv.gz
+
+  cat input/taxonMap.tsv.gz\
+  | gunzip\
+  | tail -n+2\
+  | grep -v -P ".*\t.*V\t"\
+  | gzip\
+  > output/taxonMapExcluding_Vs.tsv.gz
+
+  cat taxonMapNCBI_Vs.tsv.gz taxonMapExcluding_Vs.tsv.gz\
+  | sort\
+  | uniq\
+  > output/taxonMapNoHeader.tsv.gz
+ 
+  cat <(cat input/taxonMap.tsv.gz | gunzip | head -n1) <(cat output/taxonMapNoHeader.tsv.gz)\
+  > output/taxonMap.tsv.gz
+
+}
+
+function create_patch {
+  diff <(cat input/taxonMap.tsv.gz | gunzip) <(cat output/taxonMap.tsv.gz | gunzip) | gzip > output/taxonMap.tsv.patch.gz
+
+  cat input/taxonMap.tsv.gz | gunzip > output/taxonMapToBePatched.tsv
+  cat output/taxonMap.tsv.patch.gz | gunzip | patch -b output/taxonMapToBePatched.tsv
+  cat output/taxonMapToBePatched.tsv | gzip > output/taxonMapPatched.tsv.gz
+}
+
+init
+remove_likely_suspicious_virus_acronym_mappings
+create_patch
